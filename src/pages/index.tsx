3ecd44046d1dbd6b42d312f79a5f7a0575b05dc3
patyspro <patyspro@gmail.com>
2021-10-09 16:00:36 -0300

    finalizando o layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,12 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
-import { Button, Typography } from "@mui/material";
-import {FormElementsContainer} from  "ui/styles/pages/index.style"
+import { Button, Typography, Container } from "@mui/material";
+import {
+  FormElementsContainer,
+  ProfissionalPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
 
 const Home: NextPage = () => {
   return (
@@ -16,45 +20,65 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       ></PageTitle>
+      <Container>
       <FormElementsContainer>
-      <TextFieldMask mask={"99.999-999"} label={"Digite seu CEP"} fullWidth  variant={'outlined'}/>
-      <Typography color={'error'}> CEP inválido </Typography>
-      <Button variant={'contained'} color={'secondary'} sx={{width:'220px'}}> Buscar</Button>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
+        />
+        <Typography color={"error"}> CEP inválido </Typography>
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px" }}
+        >
+          {" "}
+          Buscar
+        </Button>
       </FormElementsContainer>
-      
-      <UserInformation
-        name={"Patricia"}
-        picture={"https://github.com/patyspro.png"}
-        rating={5}
-        description={"São Paulo"}
-      />
-      <UserInformation
-        name={"Felipe"}
-        picture={""}
-        rating={3}
-        description={"São Paulo"}
-      />
-
-      <UserInformation
-        name={"Guilherme"}
-        picture={""}
-        rating={2}
-        description={"São Paulo"}
-      />
-
-      <UserInformation
-        name={"Jorge"}
-        picture={""}
-        rating={4}
-        description={"São Paulo"}
-      />
-
-      <UserInformation
-        name={"Samuel"}
-        picture={""}
-        rating={5}
-        description={"São Paulo"}
-      />
+      <ProfissionalPaper>
+        <ProfissionaisContainer>
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+          <UserInformation
+            name={"Patricia"}
+            picture={"https://github.com/patyspro.png"}
+            rating={5}
+            description={"São Paulo"}
+          />
+        </ProfissionaisContainer>
+      </ProfissionalPaper>
+      </Container>
     </div>
   );
 };
